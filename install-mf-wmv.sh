29a178f820bdd31365fb6ab1c85fc1f18d1d4800
kubepy <43437760+kubepy@users.noreply.github.com>
2024-07-11 21:41:23 +0800

    Update install-mf-wmv.sh

--- install-mf-wmv.sh
+++ install-mf-wmv.sh
@@ -11,11 +11,11 @@ export WINEDLLPATH="$PROTON/lib/wine:$PROTON/lib64/wine"
 export WINEDEBUG="-all"
 
 if [ ! -f "windows6.1-KB976932-X64.exe" ]; then
-    wget "https://download.microsoft.com/download/0/A/F/0AFB5316-3062-494A-AB78-7FB0D4461357/windows6.1-KB976932-X64.exe"
+    wget "https://archive.org/download/windows6.1-kb976932-x64_74865ef2562006e51d7f9333b4a8d45b7a749dab/windows6.1-kb976932-x64.exe" -O windows6.1-KB976932-X64.exe
 fi
 
 if [ ! -f "Windows6.1-KB968211-x64-RefreshPkg.msu" ]; then
-    wget "http://download.microsoft.com/download/B/9/B/B9BED058-8669-490E-BA61-D502E4E8BEB1/Windows6.1-KB968211-x64-RefreshPkg.msu"
+    wget "https://web.archive.org/web/20200810163546if_/https://download.microsoft.com/download/B/9/B/B9BED058-8669-490E-BA61-D502E4E8BEB1/Windows6.1-KB968211-x64-RefreshPkg.msu"
 fi
 
 python3 installcab.py windows6.1-KB976932-X64.exe mediafoundation
